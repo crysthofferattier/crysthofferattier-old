78637b4577ecab54c05fc38a1e4a870e33dab7f1
crysthofferattier <crysthofferattier@gmail.com>
2022-01-04 01:15:09 +0000

    Start ng project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheatSheetComponent } from './cheat-sheet/cheat-sheet.component'
 
 const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CheatSheetComponent } from './cheat-sheet/cheat-sheet.component';
+import { HomeComponent } from './home/home.component';
+import { ReverseShellComponent } from './cheat-sheet/reverse-shell/reverse-shell.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'cheat-sheet', component: CheatSheetComponent},
+  {path: 'reverse-shell', component: ReverseShellComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    CheatSheetComponent,
+    HomeComponent,
+    ReverseShellComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    [RouterModule.forRoot(routes)],
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
