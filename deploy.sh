4db3e8dad46d7af2cb845c1d3b5092413e004763
crysthofferattier <crysthofferattier@gmail.com>
2022-01-04 23:30:23 +0000

    Production version 2022-01-04 23:30:23

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-ng build --prod --output-path docs --base-href="https://crysthofferattier.github.io/"
-git add . && git commit -m "Production version `date +'%Y-%m-%d %H:%M:%S'`" && git push origin main 
+ng build --prod --output-path docs --base-href="https://crysthofferattier.github.io/" \
+&& cp docs/index.html docs/404.html \
+&& git add . && git commit -m "Production version `date +'%Y-%m-%d %H:%M:%S'`" && git push origin main
